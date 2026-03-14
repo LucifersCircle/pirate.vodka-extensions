6b5dffd0281d37112562f59af322e2210def7252
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-03-13 19:11:57 -0700

    refactor(Atsumaru): change ATSUMARU_DOMAIN constant to DOMAIN

--- src/Atsumaru/services/network.ts
+++ src/Atsumaru/services/network.ts
@@ -1,11 +1,11 @@
 import type { Request, Response } from "@paperback/types";
 import { CloudflareError, PaperbackInterceptor } from "@paperback/types";
-import { ATSUMARU_DOMAIN } from "../main";
+import { DOMAIN } from "../main";
 
 export class AtsuInterceptor extends PaperbackInterceptor {
   async interceptRequest(request: Request): Promise<Request> {
     request.headers = request.headers ?? {};
-    request.headers.referer = `${ATSUMARU_DOMAIN}/`;
+    request.headers.referer = `${DOMAIN}/`;
     return request;
   }
 
